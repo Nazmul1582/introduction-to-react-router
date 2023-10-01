d6fa5766d3dd6d60e7e1ad9e1ab89012a3de35e3
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-02 00:21:53 +0600

    usecase of useNavigate hook for navigate to another route

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,21 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+
+import { useNavigate } from "react-router-dom";
 
 const Post = ({post}) => {
     const {id, title, body} = post;
+    const navigate = useNavigate();
+    
+    const handleShowDetails = postId => navigate(`/posts/${postId}`);
+
     return (
         <div className="post">
             <h3>Title: {title}</h3>
             <p>Description: {body}</p>
-            <Link to={`/posts/${id}`}>
+            {/* <Link to={`/posts/${id}`}>
                 <button>Show Details</button>
-            </Link>
+            </Link> */}
+            <button onClick={() => handleShowDetails(id)}>Show Details</button>
         </div>
     );
 };
--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,15 +1,19 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
     const post = useLoaderData();
     const {id, title, body} = post;
+    
+    const navigate = useNavigate();
+    const handleGoBack = () => navigate(-1)
     
     return (
         <div className="post" style={{width: "600px", margin: "auto", background: "#52bfff"}}>
             <h2>PostId: {id}</h2>
             <h2>Post&apos;s title: {title}</h2>
             <p>Description: {body}</p>
-            <Link to="/posts"><button>Go back</button></Link>
+            {/* <Link to="/posts"><button>Go back</button></Link> */}
+            <button onClick={handleGoBack}>Go back</button>
         </div>
     );
 };
