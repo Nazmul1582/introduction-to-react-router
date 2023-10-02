8d7051ab74465612a81bdced6cfdf51fe6918eeb
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-02 09:55:00 +0600

    changed active route color using NavLink intead using Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <div>Navbar</div>
       <nav className="nav">
-        <Link to="/home">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/contact">Contact</Link>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </nav>
     </header>
   );
