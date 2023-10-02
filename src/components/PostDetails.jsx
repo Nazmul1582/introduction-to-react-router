ae8eda55525856231ae16f78e064db789d211cd1
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-02 09:06:02 +0600

    useParams hook

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,15 +1,17 @@
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
     const post = useLoaderData();
-    const {id, title, body} = post;
+    const {title, body} = post;
     
     const navigate = useNavigate();
     const handleGoBack = () => navigate(-1)
+
+    const {postId} = useParams()
     
     return (
         <div className="post" style={{width: "600px", margin: "auto", background: "#52bfff"}}>
-            <h2>PostId: {id}</h2>
+            <h2>PostId: {postId}</h2>
             <h2>Post&apos;s title: {title}</h2>
             <p>Description: {body}</p>
             {/* <Link to="/posts"><button>Go back</button></Link> */}
