2d619a22ebca76a36d45f1bd0de7d2f60453df21
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-02 10:06:44 +0600

    added a loader before load data using useNavigation hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
+  const navigation = useNavigation();
+
   return (
     <>
       <Header />
-        <h1>Introduction To React Router</h1>
-        <Outlet />
+      <h1>Introduction To React Router</h1>
+      {navigation.state === "loading" ? <h1>Loading... </h1> : <Outlet />}
       <Footer />
     </>
   );
